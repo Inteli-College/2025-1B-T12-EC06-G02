b58989840ce04a6fc33dc68e418df04bc470c71b
Lucas-nepomuceno <lucas.santos@sou.inteli.edu.br>
2025-04-23 13:41:22 -0300

    feat: adiciona o ci/cd

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -76,7 +76,7 @@ const config = {
             items: [
               {
                 label: 'Tutorial',
-                to: '/intro',
+                to: '/',
               },
             ],
           },

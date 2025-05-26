c02309dcf700db0d4747251a462fac731a61941e
Lucas-nepomuceno <lucas.santos@sou.inteli.edu.br>
2025-05-26 14:21:25 -0300

    fix: testa a branch

--- src/sod-webapp/src/app/(components)/Uploads.jsx
+++ src/sod-webapp/src/app/(components)/Uploads.jsx
@@ -5,6 +5,7 @@ import MiniGaleria from "./miniGaleria"
 export default function Uploads({images, name, selection}){
     return(
         <Card>
+            {/*só testando o github, lembrar de implementar o upload via servidor... */}
             <Usuario nome={name}/>
             <MiniGaleria images={images}/>
         </Card>

24ec0760fb69aee60504fc937df417d49dc0e553
Lucas-nepomuceno <lucas.santos@sou.inteli.edu.br>
2025-06-01 11:06:23 -0300

    fix: permite apenas que swin seja chamado

--- src/sod-webapp/src/app/layout.js
+++ src/sod-webapp/src/app/layout.js
@@ -5,7 +5,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'SOD - Sistema de Observação de Deformação',
+  title: 'SOD - Sistema Óptico de Detecção',
   description: 'Sistema de análise de deformações estruturais',
 }
 
